f97a3ac1d6ddddbedd0bc2220074ba8b7bc4f6c8
simonsun <9262981@qq.com>
2018-06-10 21:31:13 +0800

    newAdd

--- src/java_base/Puppy2.java
+++ src/java_base/Puppy2.java
@@ -8,5 +8,19 @@ public class Puppy2 {
     public void setAge( int age){
         puppyAge = age;
     }
+    public int getAge(){
+        System.out.println("小狗的年龄是:"+puppyAge);
+        return puppyAge;
+    }
+    public static void main (String []args){
+//        创建对象
+        Puppy2 myPuppy = new Puppy2("tommy");
+//        通过方法设定age
+        myPuppy.setAge(2);
+//        调用另一个方法获取age
+        myPuppy.getAge();
+//        访问成员变量
+        System.out.println("变量值:"+myPuppy.puppyAge);
+    }
 
 }
