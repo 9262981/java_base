b76215d1d2441ac4fd15a41f71fdf4d2233a1921
simonsun <9262981@qq.com>
2018-06-10 11:00:41 +0800

    baseBegin

---
+++ src/java_base/Dog.java
@@ -0,0 +1,16 @@
+package java_base;
+
+public class Dog {
+    String breed;
+    int age;
+    String color;
+    public static void main (String []args){
+        System.out.println("barking");
+    }
+    void hungry(){
+        System.out.println("hungry");
+    }
+    void sleep(){
+        System.out.println("sleep");
+    }
+}
---
+++ src/java_base/Puppy2.java
@@ -0,0 +1,12 @@
+package java_base;
+
+public class Puppy2 {
+    int puppyAge;
+    public Puppy2(String name){
+        System.out.println("小狗的名字是:"+name);
+    }
+    public void setAge( int age){
+        puppyAge = age;
+    }
+
+}
