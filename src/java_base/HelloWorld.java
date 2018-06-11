714815922cced781fb8229d6b7eadbec8760060f
simonsun <9262981@qq.com>
2018-06-11 16:35:23 +0800

    no message

--- src/java_base/HelloWorld.java
+++ src/java_base/HelloWorld.java
@@ -1,7 +1,9 @@
 package java_base;
 
 public class HelloWorld {
+//public公开给其他类存取//class类声明//HelloWorld类名称
     public static void main (String [] args){
+//public同上//void没有返回值,空的//main方法名 //必须要传string的数组,给此方法当参数,命名为args
         System.out.println("HelloJava!");
     }
 }
---
+++ src/java_base/IfTest.java
@@ -0,0 +1,11 @@
+package java_base;
+
+class IfTest {
+    public static void main (String[] args) {
+        int x = 3;
+        if (x == 3) {
+            System.out.println("sdfsdfsfdsf");
+        }
+        System.out.println("sdfsdfsdf");
+    }
+}
