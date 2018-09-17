a3dfb3dfd64f4325a806e416dc84eb2b1b314915
simonsun <9262981@qq.com>
2018-09-17 16:45:50 +0800

    no message

--- src/HiLo/HiLo.java
+++ src/HiLo/HiLo.java
@@ -4,7 +4,7 @@ public class HiLo {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		int theNumber =  (int)(Math.random()*100+1);
 		int guess = 0;
 		System.out.println("Guess a number between 1 and 100 ! Type in next line !");
@@ -13,11 +13,8 @@ public class HiLo {
 			System.out.println("You entered " + guess + "this number is bigger than the Random number !");
 		else if (guess<theNumber)
 			System.out.println("You entered " + guess + "this number is smaller than the Random number !");
-		else 
+		else
 			System.out.println("You entered " + guess + "You guess right , You win the game !");
 		guess = scan.nextInt();
-		
-		
 	}
-
 }
